055260a5a9ec158eee1c9df9a2a97afcce374848
fatihua <fatihua@gmail.com>
2026-05-04 21:14:35 +0700

    fix: unblock ui typecheck and support app build

--- app/icon.tsx
+++
@@ -1,6 +0,0 @@
-// Icon generation - using app icon from public folder
-export default function Icon() {
-  // This file is used for generating the app icon
-  // The actual icon.svg is in the public folder
-  return null
-}
